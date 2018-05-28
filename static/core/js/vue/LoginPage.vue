765e567b835cd1d21946be43c5dbd6400b5a46c3
Nelmad <nelmadmorrey@gmail.com>
2018-05-28 03:21:58 +0300

    registration required data inputs added; registration title;

--- static/core/js/vue/LoginPage.vue
+++ static/core/js/vue/LoginPage.vue
@@ -1,105 +1,125 @@
 <template lang="html">
-  <div
-    v-if="registration.show"
-    class="registration">
+  <div style="margin-top: 100px">
+    <h2 class="login-title">{{ pageTitle }}</h2>
 
     <div
-      v-if="registration.step == 1">
-      <button
-        @click="chooseRegistrationType('teacher')">
-        Teacher
-      </button>
-      <button
-        @click="chooseRegistrationType('student')">
-        Student
-      </button>
-    </div>
+      v-if="registration.show"
+      class="registration">
 
-    <form
-      v-else
-      action="#">
-      <div v-if="registration.step == 2">
-        <div class="registration__row">
-          <input
-            type="text"
-            :placeholder="gettext('Email')"
-          >
-        </div>
-        <div class="registration__row">
-          <input
-            type="password"
-            :placeholder="gettext('Password')"
-          >
-        </div>
-      </div>
-
-      <div v-if="registration.step == 3">
-        <input type="text">
-      </div>
-
-      <div class="registration__row">
+      <div
+        v-if="registration.step == 1">
         <button
-          type="button"
-          @click="registrationHandler">
-          {{ registrationButtonText }}
+          @click="chooseRegistrationRole('teacher')">
+          Teacher
+        </button>
+        <button
+          @click="chooseRegistrationRole('student')">
+          Student
         </button>
       </div>
-    </form>
 
-    <div class="registration__row">
-      <p>I wanna
-        <a
-          href="javascript:"
-          @click="switchLogin"
-        >use an existing account</a>.
-      </p>
+      <div
+        v-else
+        class="registration__form-wrapper">
+
+        <form
+          class="registration__form"
+          action="#">
+          <div v-if="registration.step == 2">
+            <div class="registration__row">
+              <input
+                :placeholder="gettext('Email')"
+                type="text"
+              >
+            </div>
+            <div class="registration__row">
+              <input
+                :placeholder="gettext('Password')"
+                type="password"
+              >
+            </div>
+          </div>
+
+          <div v-if="registration.step == 3">
+            <div class="registration__row">
+              <input
+                :placeholder="gettext('First Name')"
+                type="text">
+            </div>
+            <div class="registration__row">
+              <input
+                :placeholder="gettext('Last Name')"
+                type="text">
+            </div>
+          </div>
+
+          <div class="registration__row">
+            <button
+              type="button"
+              @click="registrationHandler">
+              {{ registrationButtonText }}
+            </button>
+          </div>
+        </form>
+
+      </div>
+
+      <div class="registration__row registration__row--center">
+        <p>I wanna
+          <a
+            href="javascript:"
+            @click="switchLogin"
+          >use an existing account</a>.
+        </p>
+      </div>
+
     </div>
 
+    <div
+      v-else
+      class="login">
+
+      <div class="login__row">
+        <input
+          :placeholder="gettext('Email')"
+          type="text"
+          name="email">
+      </div>
+      <div class="login__row">
+        <input
+          :placeholder="gettext('Password')"
+          type="password"
+          name="password">
+      </div>
+
+      <div class="login__row">
+        <button
+          type="button"
+          @click="loginHandler">
+          Log In
+        </button>
+      </div>
+
+      <div class="login__row">
+        <p>I forgot
+          <a
+            href="javascript:"
+            @click="forgotPasswordHandler"
+          >my password</a>.
+        </p>
+      </div>
+      <div class="login__row">
+        <p>I don't have a
+          <a
+            href="javascript:"
+            @click="switchRegistration"
+          >KittenTeach account</a>.
+        </p>
+      </div>
+
+    </div>
   </div>
 
-  <div
-    v-else
-    class="login">
-
-    <div class="login__row">
-      <input
-        :placeholder="gettext('Email')"
-        type="text"
-        name="email">
-    </div>
-    <div class="login__row">
-      <input
-        :placeholder="gettext('Password')"
-        type="password"
-        name="password">
-    </div>
-
-    <div class="login__row">
-      <button
-        type="button"
-        @click="loginHandler">
-        Log In
-      </button>
-    </div>
-
-    <div class="login__row">
-      <p>I forgot
-        <a
-          href="javascript:"
-          @click="forgotPasswordHandler"
-        >my password</a>.
-      </p>
-    </div>
-    <div class="login__row">
-      <p>I don't have a
-        <a
-          href="javascript:"
-          @click="switchRegistration"
-        >KittenTeach account</a>.
-      </p>
-    </div>
-
-  </div>
 </template>
 
 <script>
@@ -110,21 +130,33 @@ export default {
     return {
       registration: {
         show: false,
-        type: '',
+        role: '',
         step: 1
       },
-      login: {
-
-      }
+      login: {}
     }
   },
 
   computed: {
     registrationButtonText: function () {
       if (this.registration.step == 2) {
-      	return 'Next'
+        return 'Next'
       } else if (this.registration.step == 3) {
-      	return 'Sign Up'
+        return 'Sign Up'
+      }
+    },
+
+    pageTitle: function () {
+      if (this.registration.show) {
+        if (this.registration.step == 1) {
+          return 'Choose your role'
+        } else if (this.registration.step == 2) {
+          return `Create a Kitten-${this.capitalizeFirstLetter(this.registration.role)}`
+        } else if (this.registration.step == 3) {
+          return 'One more minute...'
+        }
+      } else {
+        return 'Log in to KittenTeach'
       }
     }
   },
@@ -143,21 +175,28 @@ export default {
       this.registration.show = false
     },
 
-    chooseRegistrationType(type) {
-      this.registration.type = type
+    chooseRegistrationRole(role) {
+      this.registration.role = role
       this.registration.step = 2
     },
 
     registrationHandler() {
       if (this.registration.step == 2) {
-      	this.registration.step = 3
+        this.registration.step = 3
+        // TODO check if email valid and unique
       } else if (this.registration.step == 3) {
-      	console.log('registration')
+        console.log('registration')
       }
     },
 
     loginHandler() {
       console.log('login')
+    },
+
+    // TODO move to helpers
+    capitalizeFirstLetter(string) {
+      console.log(string)
+      return string.charAt(0).toUpperCase() + string.slice(1);
     }
   }
 }
