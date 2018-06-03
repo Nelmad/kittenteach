fd3496aabf2141f6fb75eee8b3028e445590b772
Nelmad <nelmadmorrey@gmail.com>
2018-06-04 01:35:47 +0300

    footer/header account page styling; header primary btn;

--- static/core/js/account.js
+++ static/core/js/account.js
@@ -3,25 +3,27 @@ $.KittenTeach.Account = {
     console.log('account init')
 
     var $header = $('header')
+    var $footer = $('footer')
     var $logo = $header.find('.header__logo-svg')
 
     var topStyleActivated = true
     $header.addClass('header--account')
     $logo.addClass('header__logo-svg--brown')
+    $footer.addClass('footer--account')
 
     $(window).on('scroll', function (e) {
       if ($(this).scrollTop() <= 45) {
         if (!topStyleActivated) {
           $header.addClass('header--account')
-          $logo.removeClass('header__logo-svg--white')
-          $logo.addClass('header__logo-svg--brown')
+          $header.removeClass('header--account-secondary')
+
           topStyleActivated = true
         }
       } else {
         if (topStyleActivated) {
           $header.removeClass('header--account')
-          $logo.removeClass('header__logo-svg--brown')
-          $logo.addClass('header__logo-svg--white')
+          $header.addClass('header--account-secondary')
+
           topStyleActivated = false
         }
       }
